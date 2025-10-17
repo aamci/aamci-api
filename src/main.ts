b9b52f7227592dc480002655120165a24a75068c
Marvel Lawson <lawsonmarvel@gmail.com>
2025-10-17 22:00:58 +0200

    fixed the exposed port

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 async function bootstrap(){
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin:true, credentials:true });
-  await app.listen(process.env.PORT || 3001);
+  await app.listen(process.env.PORT ? Number(process.env.PORT) : 3000, '0.0.0.0');
 }
 bootstrap();
