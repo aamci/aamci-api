2fd1448b0c96ab86bb43c95e59a3e39e42d8d894
Marvel Lawson <lawsonmarvel@gmail.com>
2025-10-17 22:36:14 +0200

    fixed the port issue

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 async function bootstrap(){
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin:true, credentials:true });
-  await app.listen(process.env.PORT ? Number(process.env.PORT) : 3000, '0.0.0.0');
+  // Bind to 0.0.0.0 so it's reachable outside the container
+  const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`🚀 API running on: http://0.0.0.0:${port}`);
 }
 bootstrap();
